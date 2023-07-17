b9df53ba176bc94b65b50417d4612d23b1bb61d9
crackalamoo <harysdalvi@gmail.com>
2023-07-16 22:15:17 -0400

    separate rhyme/meter encoding into a new function

--- model.py
+++ model.py
@@ -163,6 +163,19 @@ class TransformerModel(keras.Model):
         return pred
 
 
+def rhyme_meter_encoding(input):
+    vowels = input[:,:,:RHYME_STACK_SIZE]
+    consonants = input[:,:,RHYME_STACK_SIZE:RHYME_STACK_SIZE*2]
+    meter = input[:,:,-METER_STACK_SIZE:]
+    vowels = tf.one_hot(vowels, VOWEL_TYPES)
+    consonants = tf.one_hot(consonants, CONSONANT_TYPES)
+    vowels = tf.reshape(vowels, [tf.shape(vowels)[0], tf.shape(vowels)[1], -1])
+    consonants = tf.reshape(consonants, [tf.shape(consonants)[0], tf.shape(consonants)[1], -1])
+    rhyme = tf.concat([vowels, consonants], axis=2)
+    meter = tf.cast(meter, tf.float32)
+    rhyme_meter = tf.concat([rhyme, meter], axis=2)
+    return rhyme_meter
+
 class BardModel(keras.Model):
     def __init__(self, *, num_layers=TRANSFORMER_LAYERS, num_heads=TRANSFORMER_HEADS, dff=TRANSFORMER_DFF):
         super(BardModel, self).__init__()
@@ -175,7 +188,7 @@ class BardModel(keras.Model):
         self.rhyme_meter_pred = keras.Sequential([
             # input is context x rhyme/meter encoding
             Dense(RHYME_METER_DFF, activation='relu'), # context x 16
-            Dense(RHYME_METER_DFF, activation='tanh'),
+            Dense(RHYME_METER_DFF),
             Dense(VOCAB_SIZE) # context x vocab size (to match transformer output)
         ], name='rhyme_meter')
         self.add = Add()
@@ -189,16 +202,7 @@ class BardModel(keras.Model):
             del x._keras_mask
         except AttributeError:
             pass
-        vowels = input[1][:,:,:RHYME_STACK_SIZE]
-        consonants = input[1][:,:,RHYME_STACK_SIZE:RHYME_STACK_SIZE*2]
-        meter = input[1][:,:,-METER_STACK_SIZE:]
-        vowels = tf.one_hot(vowels, VOWEL_TYPES)
-        consonants = tf.one_hot(consonants, CONSONANT_TYPES)
-        vowels = tf.reshape(vowels, [tf.shape(vowels)[0], tf.shape(vowels)[1], -1])
-        consonants = tf.reshape(consonants, [tf.shape(consonants)[0], tf.shape(consonants)[1], -1])
-        rhyme = tf.concat([vowels, consonants], axis=2)
-        meter = tf.cast(meter, tf.float32)
-        rhyme_meter = tf.concat([rhyme, meter], axis=2)
+        rhyme_meter = rhyme_meter_encoding(input[1])
         rhyme_meter_x = self.rhyme_meter_pred(rhyme_meter)
         x = self.add([x, rhyme_meter_x])
         x = self.softmax(x)
